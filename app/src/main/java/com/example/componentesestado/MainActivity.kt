e934d1ca241199a21628205eba4ae92bc008d0da
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246733.cfp127aluno.local>
2026-09-17 14:22:09 -0300

    feat: finalizar exemplo

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
 import com.example.componentesestado.ui.theme.Righteous
@@ -50,14 +57,43 @@ fun BasicComponentScreen(modifier: Modifier = Modifier) {
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
             color = Color(0xFF3DDC84),
-            fontFamily = FontFamily.Serif
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.End,
+            letterSpacing = 4.sp
         )
         Text(
             text = "com Jetpack Compose",
             fontSize = 16.sp,
             fontWeight = FontWeight.SemiBold,
             color = Color(66, 133, 244),
-            fontFamily = Righteous
+            fontFamily = Righteous,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+
+        AndroidEnemy(
+            modifier = Modifier.size(100.dp),
+            color = Color.Red
+        )
+
+        AndroidEnemy(
+            modifier = Modifier.size(100.dp),
+            color = Color.Black
         )
     }
+}
+
+@Composable
+fun AndroidEnemy(
+    modifier: Modifier = Modifier,
+    color: Color
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        colorFilter = ColorFilter.tint(color),
+        contentDescription = "Android Enemy"
+    )
 }
